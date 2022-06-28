eb504c4446513c6b7b2d8c39d0e63d8beeaebbcb
Reteam.id <81605818+reza838@users.noreply.github.com>
2022-06-27 21:00:18 -0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [0831-6398-9496]
+│ •  [0831-6398-9500]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
