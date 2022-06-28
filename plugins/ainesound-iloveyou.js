5d4066f3014cebf50c2705c00fd2d8560afbb992
Reteam.id <81605818+reza838@users.noreply.github.com>
2022-06-28 10:42:45 +0700

    Update ainesound-iloveyou.js

--- plugins/ainesound-iloveyou.js
+++ plugins/ainesound-iloveyou.js
@@ -1,4 +1,4 @@
-/*let fs = require('fs')
+let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m) => {
 let helloaine = fs.readFileSync('./mp3/PTT-20211218-WA0243.opus') 
@@ -14,4 +14,4 @@ handler.premium = false
 handler.group = true
 handler.private = false
 
-module.exports = handler*/
+module.exports = handler
