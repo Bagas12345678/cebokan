585a4b29bd46bd5f0115237d3f2bea8f606a0dc6
Reteam.id <81605818+reza838@users.noreply.github.com>
2022-06-28 10:22:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'Wa bot'
-  var sticker_author = 'ig reteam.id'
+  var sticker_name = '2022\n'
+  var sticker_author = '\nig reteam.id'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Aine'
+global.wm = 'ig : reteam.id'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
